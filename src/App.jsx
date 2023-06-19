360a151ddb08a1420394edf7cc30a4498890238d
Lucas Rossi <lucas_lnr@hotmail.com>
2023-06-18 22:44:55 -0300

    Comienzo de trabajo con Jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 
+import { Navbar } from "./Navbar"
+import Footer from "./Footer"
+import { Home } from "./Home";
+
+
 function App() {
 
-  return <h1>Hola mundo en React!</h1>
-
+  return (
+    <div>
+      <Navbar />
+      <h1>Este es el app</h1>
+      <Footer />
+      <Home />
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+
+
+
+
+// Navbar()
+// <Navbar />
+
+
+// const sumar = ()=>{
+
+//   return "hola como estas"
+// }
+
+// sumar() // ---> "hola como estas"
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+
+import "./Footer.css"
+
+const Footer = ()=>{
+    
+    return <h1 className="title">Este es el Footer</h1>
+}
+
+export default Footer
